f586220b7cc7bad92a9beb66f73f6ee42b999bae
zzdiniz <bruno7240@gmail.com>
2024-03-06 20:50:43 -0300

    feat: add sample express template

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-interface User {
-    name: string
-    age: number
-}
-
-const saveUser = (user:User) =>{
-    console.log(user, typeof user)
-}
-
-
-const user = {name:'bruno', age:21}
-
-saveUser(user)
+import express, {Request,Response}from "express"
+import User from "./models/User"
+const app = express()
+const PORT = 3000
+const user:User = {name:'Bruno', age:21}
+app.get('/', (req,res)=>{
+    res.send('Hello world!')
+})
+app.get('/user',(req:Request,res:Response)=>{
+    res.send(`My name is ${user.name} and i'm ${user.age} years old :)`)
+})
+app.listen(PORT,()=>{
+    console.log(`Listening to port ${PORT}`)
+})
